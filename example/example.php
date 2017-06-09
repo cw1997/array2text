815a50dd0061f6bf3e90b29d9e8d06147b202291
昌维 <867597730@qq.com>
2017-06-09 20:40:48 +0800

    修正example中的input数组

--- example/example.php
+++ example/example.php
@@ -3,15 +3,12 @@
 $arr[0]['id'] = 1;
 $arr[0]['title'] = '一';
 $arr[0]['author'] = 'h';
-$arr[1]['id'] = 22;
-$arr[1]['title'] = '二二';
-$arr[1]['author'] = 'hh';
-$arr[2]['id'] = 333; // 13
-$arr[2]['title'] = '三三三'; // 12
-$arr[2]['author'] = 'hhh'; // 10
 $arr[1]['id'] = 1997;
 $arr[1]['title'] = '一个数组转文本的小工具';
 $arr[1]['author'] = '昌维changwei';
+$arr[2]['id'] = 333; // 13
+$arr[2]['title'] = '三三三'; // 12
+$arr[2]['author'] = 'hhh'; // 10
 
 require_once __DIR__ . './../vendor/autoload.php';
 
